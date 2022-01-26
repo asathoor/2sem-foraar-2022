e1eb495286b2b1d3f7b83942356cc062d83d2b5e
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2022-01-26 20:05:38 +0100

    Update js.js

--- javascriptBoilerPlates/js/js.js
+++ javascriptBoilerPlates/js/js.js
@@ -3,10 +3,3 @@
  * purpose: interactivity
  **/
 console.log('JavaScript from js/js.js: up and running!');
-
-// Tjek at DOM er indlæst
-window.onload(function () {
-    // ... your code here ...
-    
-    // ... end ...
-});
