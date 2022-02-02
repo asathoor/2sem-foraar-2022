1678495263d175b09359f28e6e510c4a3e19084e
Per Thykjaer Jensen <per@multimusen.dk>
2022-02-02 14:45:44 +0100

    d-dag-6-feb-01

--- d-dag-6-feb-01/js/if-eksempler.js
+++ d-dag-6-feb-01/js/if-eksempler.js
@@ -2,32 +2,21 @@
 console.log('hej fra if eksempler');
 
 // bilen kører med
-let hastighed = 60;
+let hastighed = prompt("km/t");
 
 // kører vi for stærkt?
-
-if ( hastighed < 45 ) {
+if ( hastighed <= 45 ) {
   resultat.innerHTML = "Do kører for langsomt!"
 }
 
+if ( hastighed <= 90 ) {
+  resultat.innerHTML = "Yearh vi kører"
+}
+
+if ( hastighed >= 130 ) {
+  resultat.innerHTML = "Let på speederen!"
+}
+/*
 else {
   resultat.innerHTML = "Do kører derudaf!"
-}
-
-// carlsberg test
-var alder = 19;
-if (alder < 18) text = "Too young to buy alcohol";
-else {
-  resulat.innerHTML += "Velkommen"
-}
-
-
-/*
-if ( hastighed <= 60 ) {
-  resultat.innerHTML = "Du kører med en passende hastighed"
-}
-
-else {
-  resultat.innerHTML = "Do kører for hurtigt!"
-}
-*/
+}*/
